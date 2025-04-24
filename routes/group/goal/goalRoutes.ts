e2026f0a2d8503a6da3c80869836c10076601734
DenFlyvendeGed <simont1998@gmail.com>
2025-04-24 15:32:26 +0200

    added goal patch endpoint

--- routes/group/goal/goalRoutes.ts
+++ routes/group/goal/goalRoutes.ts
@@ -60,9 +60,9 @@ router.delete('/', async (req: Request, res: Response) => {
         const goalId = parseRes.uuid;
 
         //Check if user should be able to delete goalId
-        const groupObjArray =  
-            await (await getFilter(collectionEnum.Group, { users: userId }, { _id: 1 })
-        ).toArray()
+        const groupObjArray = await (
+            await getFilter(collectionEnum.Group, { users: userId }, { _id: 1 })
+        ).toArray();
         const groupIdArray: Array<string> = [];
         for (const object of groupObjArray) {
             groupIdArray.push(object._id.toString());
@@ -74,20 +74,20 @@ router.delete('/', async (req: Request, res: Response) => {
 
         parseOutput(GoalDeleteRequestSchema, {}, res);
     }
-})
+});
 
 //Create individual goal
 router.post('/individual', async (req: Request, res: Response) => {
-    const parseRes = parseInput(IndividualGoalCreateRequestSchema, req, res)
-    if(parseRes.success){
+    const parseRes = parseInput(IndividualGoalCreateRequestSchema, req, res);
+    if (parseRes.success) {
         const individualGoal = {
             title: parseRes.title,
             activity: parseRes.activity,
-            metric: parseRes.metric, 
+            metric: parseRes.metric,
             target: parseRes.target,
             user: parseRes.user,
-            progress: 0
-        }
+            progress: 0,
+        };
         if (
             await existFilter(collectionEnum.Group, {
                 _id: parseRes.group,
@@ -95,7 +95,9 @@ router.post('/individual', async (req: Request, res: Response) => {
             })
         ) {
             const response = {
-                uuid: (await insert(collectionEnum.Goal, individualGoal)).toString(),
+                uuid: (
+                    await insert(collectionEnum.Goal, individualGoal)
+                ).toString(),
             };
             parseOutput(IndividualGoalCreateRequestSchema, response, res);
         } else {
@@ -105,27 +107,51 @@ router.post('/individual', async (req: Request, res: Response) => {
 });
 
 router.delete('/individual', async (req: Request, res: Response) => {
-    const parseRes = parseInput(GoalDeleteRequestSchema, req, res)
-    if(parseRes.success){
-        
+    const parseRes = parseInput(GoalDeleteRequestSchema, req, res);
+    if (parseRes.success) {
     }
 });
 
 //Patch goal
-router.patch('/', async ( req: Request, res: Response) =>{
-    const parseRes = parseInput(GoalPatchRequestSchema, req, res)
-    if(parseRes.success){
-        //check if group or individual
-        console.log(parseRes)
-        if(safeParse(GroupGoalSchema,parseRes).success){
-            console.log("Group!")
+router.patch('/', async (req: Request, res: Response) => {
+    const parseRes = parseInput(GoalPatchRequestSchema, req, res);
+    if (parseRes.success) {
+        const goals = parseRes.body.reduce((obj, item) => {
+            obj[item.uuid] = item.progress;
+            return obj;
+        }, {});
+
+        //This will drop any goals that the user should not have acces to, but will do so silently
+        const idObjArray = await (
+            await getFilter(
+                collectionEnum.Group,
+                { users: parseRes.user, goals: { $in: Object.keys(goals) } },
+                { _id: 0, goals: 1 },
+            )
+        ).toArray();
+
+        const validGoalIds = idObjArray.reduce(
+            (accumulator, value) => accumulator.concat(value.goals),
+            [],
+        );
+        const goalObjArray = await (
+            await getFilter(collectionEnum.Goal, { _id: { $in: validGoalIds } })
+        ).toArray();
+
+        for (const goal of goalObjArray) {
+            if ('user' in goal) {
+                //idividual goal
+                update(collectionEnum.Goal, goal._id, {
+                    $inc: { progress: goals[goal._id] },
+                });
+            } else {
+                //group goal
+                const field: string = 'progress.' + parseRes.user;
+                update(collectionEnum.Goal, goal._id, {
+                    $inc: { [field]: goals[goal._id] },
+                });
+            }
         }
-        else if(safeParse(GroupSchema, parseRes).success){
-            console.log("Individual!")
-        }
-        else{
-            console.log(":(")
-            //Should not be possible
-        }
+        parseOutput(GoalPatchRequestSchema, {}, res);
     }
-})
+});
