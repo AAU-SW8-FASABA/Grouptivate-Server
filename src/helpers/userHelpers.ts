f780d5be493e5c55527f75ff8082d5277c1a5302
Bastian Hansen <bastian.b.hansen@gmail.com>
2025-05-01 17:11:28 +0200

    Format

--- src/helpers/userHelpers.ts
+++ src/helpers/userHelpers.ts
@@ -1,27 +1,27 @@
-import UserModel from '../models/UserModel';
-import GroupModel from '../models/GroupModel';
+import UserModel from "../models/UserModel";
+import GroupModel from "../models/GroupModel";
 
 export async function getUserIdByName(
-    name: string,
+	name: string,
 ): Promise<string | undefined> {
-    const user = await UserModel.findOne({ name });
-    return user?.id;
+	const user = await UserModel.findOne({ name });
+	return user?.id;
 }
 
 export async function getNameById(id: string): Promise<string | undefined> {
-    const user = await UserModel.findById(id);
-    return user?.name;
+	const user = await UserModel.findById(id);
+	return user?.name;
 }
 
 export async function getGroupNameById(
-    id: string,
+	id: string,
 ): Promise<string | undefined> {
-    const group = await GroupModel.findById(id);
-    return group?.name;
+	const group = await GroupModel.findById(id);
+	return group?.name;
 }
 
 export async function getUserMap(ids: string[]): Promise<Map<string, string>> {
-    const users = await UserModel.find({ _id: { $in: ids } });
+	const users = await UserModel.find({ _id: { $in: ids } });
 
-    return new Map(users.map((user) => [user.id, user.name]));
+	return new Map(users.map((user) => [user.id, user.name]));
 }
