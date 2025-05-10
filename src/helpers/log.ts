2824ff323a0828040008ac9ab2b7770d6ef52826
Nyby <nyby.dev@gmail.com>
2025-05-10 10:54:56 +0200

    file loggings

--- src/helpers/log.ts
+++ src/helpers/log.ts
@@ -1,5 +1,20 @@
 import { type Request } from "express";
 import cluster from "node:cluster";
+import winston, { format } from "winston";
+
+const logger = winston.createLogger({
+	level: "error",
+	format: format.combine(
+		format((data) => {
+			if (data.details) {
+				data.message += " " + JSON.stringify(data.details);
+			}
+			return data;
+		})(),
+		format.printf(({ message }) => String(message)),
+	),
+	transports: [new winston.transports.File({ filename: "errors.log" })],
+});
 
 export default function logRequest(req: Request, ...messages: any[]) {
 	const prettyMessages = messages.reduce<any[]>((acc, curr, index) => {
@@ -12,5 +27,9 @@ export default function logRequest(req: Request, ...messages: any[]) {
 	console.log(
 		`❌ [${new Date(Date.now()).toLocaleString()} - ${cluster.isWorker ? cluster.worker?.id : 0}] ${req.method} @ ${req.originalUrl} - ${req.ip}:`,
 		...prettyMessages,
+	);
+	logger.error(
+		`❌ [${new Date(Date.now()).toLocaleString()} - ${cluster.isWorker ? cluster.worker?.id : 0}] ${req.method} @ ${req.originalUrl} - ${req.ip}:`,
+		{ details: prettyMessages },
 	);
 }
